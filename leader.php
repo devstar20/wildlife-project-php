e25434050131d10e482034871241591b54fbb93a
devstart20 <ninjadev2020@gmail.com>
2019-11-01 05:15:44 -0700

    Create leader.php

---
+++ leader.php
@@ -0,0 +1,64 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Leader</title>
+        <link rel="icon" href="reindeer.png" type="image/png">
+        <link rel="stylesheet" href="css/leader.css">
+        <style></style>
+        <script> </script>
+    <?php include_once 'includes/head-end-tag.php';?>
+
+    <body>
+        <div id="loading"></div>
+        <?php include_once 'includes/header.php';?>
+
+        <div class="scroll-slide">
+                <div class="scroll-1"><img src="images-leader\Scroll Slides\Cane Toad.jpg"><img src="images-leader\Scroll Slides\Dingo.jpg"><img src="images-leader\Scroll Slides\Echidna.jpg"><img src="images-leader\Scroll Slides\Goanna.jpg"><img src="images-leader\Scroll Slides\Inland Taipan.jpg"><img src="images-leader\Scroll Slides\Kangaroo.jpg"><img src="images-leader\Scroll Slides\Koala.jpg"><img src="images-leader\Scroll Slides\Macrotis.jpg"><img src="images-leader\Scroll Slides\Platypus.jpg"><img src="images-leader\Scroll Slides\Tasmanian Devil.jpg"><img src="images-leader\Scroll Slides\Thorny Dragon.jpg"><img src="images-leader\Scroll Slides\Tiger Quoll.jpg"><img src="images-leader\Scroll Slides\Wallaby.jpg"><img src="images-leader\Scroll Slides\Wombat.jpg"><img src="images-leader\Scroll Slides\Cane Toad.jpg"><img src="images-leader\Scroll Slides\Dingo.jpg"><img src="images-leader\Scroll Slides\Echidna.jpg"><img src="images-leader\Scroll Slides\Goanna.jpg"><img src="images-leader\Scroll Slides\Inland Taipan.jpg"><img src="images-leader\Scroll Slides\Kangaroo.jpg"><img src="images-leader\Scroll Slides\Koala.jpg"><img src="images-leader\Scroll Slides\Macrotis.jpg"><img src="images-leader\Scroll Slides\Platypus.jpg"><img src="images-leader\Scroll Slides\Tasmanian Devil.jpg"><img src="images-leader\Scroll Slides\Thorny Dragon.jpg"><img src="images-leader\Scroll Slides\Tiger Quoll.jpg"><img src="images-leader\Scroll Slides\Wallaby.jpg"><img src="images-leader\Scroll Slides\Wombat.jpg"></div>
+                <div class="scroll-2"><img src="images-leader\Scroll Slides\1.jpg"><img src="images-leader\Scroll Slides\2.jpg"><img src="images-leader\Scroll Slides\3.jpg"><img src="images-leader\Scroll Slides\4.jpg"><img src="images-leader\Scroll Slides\1.jpg"><img src="images-leader\Scroll Slides\2.jpg"><img src="images-leader\Scroll Slides\3.jpg"><img src="images-leader\Scroll Slides\4.jpg"><img src="images-leader\Scroll Slides\1.jpg"><img src="images-leader\Scroll Slides\2.jpg"><img src="images-leader\Scroll Slides\3.jpg"><img src="images-leader\Scroll Slides\4.jpg"><img src="images-leader\Scroll Slides\1.jpg"><img src="images-leader\Scroll Slides\2.jpg"><img src="images-leader\Scroll Slides\3.jpg"><img src="images-leader\Scroll Slides\4.jpg"><img src="images-leader\Scroll Slides\1.jpg"><img src="images-leader\Scroll Slides\2.jpg"><img src="images-leader\Scroll Slides\3.jpg"><img src="images-leader\Scroll Slides\4.jpg"><img src="images-leader\Scroll Slides\1.jpg"><img src="images-leader\Scroll Slides\2.jpg"><img src="images-leader\Scroll Slides\3.jpg"><img src="images-leader\Scroll Slides\4.jpg"><img src="images-leader\Scroll Slides\1.jpg"><img src="images-leader\Scroll Slides\2.jpg"><img src="images-leader\Scroll Slides\3.jpg"><img src="images-leader\Scroll Slides\4.jpg"></div>
+        </div>
+
+        <section class="leader-section">
+            <p id="main-topic">Three Topics</p>
+            <div class="leader-div">
+                <div>
+                    <a class="caption" href="wildlifeinfo.php">Know Animals</a>
+                    <a href="wildlifeinfo.php" class="topic">
+                        <img alt="Know Animals" src="images-leader/1.png">
+                        <div class="topic-caption">Know Animals</div>
+                    </a>
+                </div>
+                <div>
+                    <a class="caption" href="#">Become Worse</a>
+                    <a href="#" class="topic">
+                        <img alt="Become Worse" src="images-leader/2.png">
+                        <div class="topic-caption">Become Worse</div>
+                    </a>
+                </div>
+                <div>
+                    <a class="caption" href="extinction.php">No Animals</a>
+                    <a href="extinction.php" class="topic">
+                        <img alt="No Animals" src="images-leader/3.png">
+                        <div class="topic-caption">No Animals</div>
+                    </a>
+                </div>
+            </div>
+
+        </section>
+
+        <footer>
+            <!--<hr style="border-width:2px;border-style:solid;border-color:red;margin:0 0 20px 0;">-->
+            <div class="footer-container">
+                <p><a href="">About us</a></p>
+            </div>
+        </footer>
+        <script src="js/jquery-3.4.0.min.js"></script>
+        <script>
+            setTimeout(function () {
+                $("body").addClass("loaded");
+            }, 2000); // x-second delay. Unit: ms.
+        </script>
+        <!--<script src="js/strip.pkgd.min.js"></script>-->
+    </body>
+</html>
