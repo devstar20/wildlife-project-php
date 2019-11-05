6ef851aba7b4cf5bd171a95ac99788c711798b50
devstart20 <ninjadev2020@gmail.com>
2019-11-05 00:49:42 -0800

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<header class="">
+            <div id="logo"><img src="wildlife_logo.png"></div>
+            <div id="help1"></div>
+            <nav class="">
+                <ul class="">
+                    <li><a href="leader.php">Home</a></li>
+                    <li><a href="index.php">Animation</a></li>
+                    <li><a href="puzzle.php">Puzzle</a></li>
+                    <li><a href="searchImageEngine.php">Image search</a></li>
+                    <li><a href="quiz.php">Quiz</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </nav>
+            <!--<div id="form">
+            <form action="/action_page.php">
+                <div id="user">User name: <input type="text" name="UserName" placeholder="s1234567"></div>
+                <div id="pw">Password: <input type="text" name="Password" placeholder="7654321"></div>
+                <div id="login"><input type="submit" value="Login"></div>
+            </form>
+            </div>-->
+        </header>
